a5d3de22c8ad1bc9fd4f587cc15bbc43c730f1c5
Elaine Yuan <elaine.yuan@theinformationlab.com>
2026-09-15 15:58:19 +0000

    align project to dbt's recommended materializations and refactor

--- analyses/mart_weather_validation.sql
+++ analyses/mart_weather_validation.sql
@@ -1,6 +1,5 @@
 --does mart_weather have one row per location per day per hour?
 --ideal=0 rows returned
-/*
 SELECT
     LOCATIONID,
     WEATHER_DATE,
@@ -12,23 +11,22 @@ GROUP BY
     WEATHER_DATE,
     WEATHER_TIME
 HAVING COUNT(*)>1
-*/
+
 
 --do the number of records match between int_hourly_weather and mart_weather?
-/*
 SELECT COUNT(*) AS hourly_rows
 FROM INT_HOURLY_WEATHER
-*/
+
 --11470 - last date 8/27
 
-/*
+
 SELECT COUNT(*) AS mart_rows
 FROM MART_WEATHER
-*/
+
 --1140 - last date 8/27
 
+
 -- end-to-end check
-/*
 SELECT
     d.LOCATION,
     COUNT(DISTINCT f.WEATHER_DATE) AS daily_dates,
@@ -39,10 +37,9 @@ FROM INT_HOURLY_WEATHER f
 JOIN DIM_LOCATION d
 ON f.LOCATIONID = d.LOCATIONID
 GROUP BY d.LOCATION
-*/
+
 
 --general preview
-
 SELECT *
 FROM MART_WEATHER
 ORDER BY WEATHER_DATE DESC, WEATHER_TIME ASC
--- analyses/stg_daily_weather_validation.sql
+++ analyses/stg_daily_weather_validation.sql
@@ -1,30 +1,28 @@
 --does int_daily_weather have one row per location per day?
 --ideal=0 rows returned
-/*
 SELECT
-    LOCATIONID,
+    LOCATION,
     WEATHER_DATE,
     COUNT(*) AS row_count
-FROM INT_DAILY_WEATHER
+FROM STG_DAILY_WEATHER
 GROUP BY
-    LOCATIONID,
+    LOCATION,
     WEATHER_DATE
 HAVING COUNT(*)>1
-*/
+
 
 --is everything populating as expected?
-/*
 SELECT
     COUNT(*) AS total_rows,
-    COUNT(DISTINCT LOCATIONID||'|'||WEATHER_DATE) AS unique_location_days,
-    COUNT(LOCATIONID) AS location_populated,
+    COUNT(DISTINCT LOCATION||'|'||WEATHER_DATE) AS unique_location_days,
+    COUNT(LOCATION) AS location_populated,
     COUNT(WEATHER_DATE) AS date_populated,
     COUNT(TEMPMAX) AS tempmax_populated,
     COUNT(TEMPMIN) AS tempmin_populated
-FROM INT_DAILY_WEATHER
-*/
+FROM STG_DAILY_WEATHER
+
 
 --general preview
 SELECT *
-FROM INT_DAILY_WEATHER
+FROM STG_DAILY_WEATHER
 ORDER BY WEATHER_DATE DESC
